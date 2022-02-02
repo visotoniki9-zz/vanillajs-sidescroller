bac07c6d8ba5b61c769031783a5f04e598710715
visotoniki9 <d.simon.ssx0@gmail.com>
2022-02-02 20:14:53 +0100

    Added spacebar for jumping

--- src/classes/InputHandler.js
+++ src/classes/InputHandler.js
@@ -5,16 +5,19 @@ export default class InputHandler {
       if ((e.key === 'w'
       || e.key === 'a'
       || e.key === 's'
-      || e.key === 'd')
+      || e.key === 'd'
+      || e.key === ' ')
       && this.keys.indexOf(e.key) === -1) {
         this.keys.push(e.key);
       }
+      console.log(this.keys);
     });
     window.addEventListener('keyup', (e) => {
       const index = this.keys.indexOf(e.key);
       if (index !== -1) {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
+      console.log(this.keys);
     });
   }
 }
